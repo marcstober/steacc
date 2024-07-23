23825aa1b07f84ba8d8154a58fefc19567b92a6c
Marc Stober <marcstober@gmail.com>
2024-07-23 13:08:37 -0400

    proposed changes from B and J, including launching webboard

---
+++ installExtention.ps1
@@ -0,0 +1,8 @@
+# Made with <3 by GustyCube (Bennett Schwartz) and Joshua Kellman
+Write-Output "Installing P5-VSCode"
+code --install-extension samplavigne.p5-vscode
+Write-Output "Installed P5-VSCode"
+Write-Output "Installing Live Server"
+code --install-extension ritwickdey.liveserver
+Write-Output "Installed Live Server"
+pause
--- run-winget.ps1
+++ run-winget.ps1
@@ -1,3 +1,7 @@
+# Made with <3 by GustyCube (Bennett Schwartz) and Joshua Kellman
+Write-Output "Installing Windows Terminal"
 winget install -e --id Microsoft.WindowsTerminal
+Write-Output "Installing Visual Studio Code"
 winget install -e --id Microsoft.VisualStudioCode
+Write-Output "App install complete."
 pause
