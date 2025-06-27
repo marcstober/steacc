1687a4be56cf0b16be47e1043b81f716305c0e1b
Marc Stober <marcstober@gmail.com>
2025-06-26 21:22:25 -0400

    remove uninstall stuff that didn't actually work

--- postinstall.ps1
+++ postinstall.ps1
@@ -1,6 +1,5 @@
 $nodeBinDir = Join-Path $home 'AppData\Roaming\npm'
 Write-Host "Installing S.T.E.A.C.C. wrapper script to global npm bin directory: $nodeBinDir"
-# anything installed here should be removed in preuninstall.ps1
 Copy-Item .\steacc.ps1 $nodeBinDir -PassThru | ForEach-Object {
     Write-Host "Copied: $($_.FullName)"
 }
